0ed88de60ce66b80e3943bce03a4ccec625a286c
dtejwani <55606729+dtejwani@users.noreply.github.com>
2020-06-23 05:44:48 +0530

    Third Commit

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,55 +2,82 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button play, pause, stop;
     MediaPlayer mediaPlayer;
     int pauseCurrent;
-
+    int sec=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        play= (Button) findViewById(R.id.btn_play);
-        stop= (Button) findViewById(R.id.btn_stop);
-        pause= (Button) findViewById(R.id.btn_pause);
-        play.setOnClickListener(this);
-        pause.setOnClickListener(this);
-        stop.setOnClickListener(this);
+        TextView song1=(TextView) findViewById(R.id.song1);
+        song1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent songplayers= new Intent(MainActivity.this, songplay.class);
+                sec=0;
+                songplayers.putExtra("songsss", sec);
+                startActivity(songplayers);
 
-        }
+            }
+        });
+        TextView song2= findViewById(R.id.song2);
+        TextView song3= findViewById(R.id.song3);
+        TextView song4= findViewById(R.id.song4);
+        TextView song5= findViewById(R.id.song5);
+        TextView song6= findViewById(R.id.song6);
+        TextView song7= findViewById(R.id.song7);
+        song2.setOnClickListener(this);
+        song3.setOnClickListener(MainActivity.this);
+        song4.setOnClickListener(MainActivity.this);
+        song5.setOnClickListener(MainActivity.this);
+        song6.setOnClickListener(MainActivity.this);
+        song7.setOnClickListener(MainActivity.this);
+        Button bestsongs= findViewById(R.id.bestsong);
+        bestsongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,bestsong.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
     @Override
-    public void onClick(View view) {
-
-        switch (view.getId()){
-            case R.id.btn_play:
-                if(mediaPlayer==null){
-                mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.music);
-                mediaPlayer.start();}
-
-                else if (!mediaPlayer.isPlaying()){
-                    mediaPlayer.seekTo(pauseCurrent);
-                    mediaPlayer.start();
-                }
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.song2:
+                sec=2;
                 break;
-            case R.id.btn_pause:
-                if (mediaPlayer!=null) {
-                    mediaPlayer.pause();
-                    pauseCurrent = mediaPlayer.getCurrentPosition();
-                }
+            case R.id.song3:
+                sec=3;
                 break;
-            case R.id.btn_stop:
-                if(mediaPlayer!=null){
-                mediaPlayer.stop();}
-                mediaPlayer=null;
+            case R.id.song4:
+                sec=4;
+                break;
+            case R.id.song5:
+                sec=5;
+                break;
+            case R.id.song6:
+                sec=6;
+                break;
+            case R.id.song7:
+                sec=7;
                 break;
         }
+        Intent songplayers= new Intent(MainActivity.this, songplay.class);
+        songplayers.putExtra("songsss", sec);
+        startActivity(songplayers);
+        sec=0;
     }
 }
